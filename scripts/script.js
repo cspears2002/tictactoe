82362c9753d8f9b180a84e7b7d671bcb384e7154
cspears2002 <cspears2002@yahoo.com>
2013-09-25 18:15:24 -0700

    Place restart button

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,9 @@ function addXO(element){
 	var rd_plyer_1 = document.getElementById('player_1');
 	var rd_plyer_2 = document.getElementById('player_2');
 
-	if (rd_plyer_1.checked)
+	if (rd_plyer_1.checked && element.innerHTML != "O")
 		element.innerHTML = "X";
 
-	if (rd_plyer_2.checked)
+	if (rd_plyer_2.checked && element.innerHTML != "X")
 		element.innerHTML = "O";
 }
