fc00574c32fa27c823f61de45ae9dba2bfab4356
cspears2002 <cspears2002@yahoo.com>
2013-09-25 17:37:28 -0700

    Set up radio buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,11 @@
 function addXO(element){
 	//console.log(element)
-	element.innerHTML = "X"
+	var radios = document.getElementsByName('player');
+
+	if (radios.value == "player_1")
+		element.innerHTML = "X";
+	else if (radios.value = "player_2")
+		element.innerHTML = "O";
+	else
+		element.innerHTML = "There can only be one.";
 }
