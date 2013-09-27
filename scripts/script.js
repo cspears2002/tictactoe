ce3870c14f39d1bbe0cc8854c5858b8b782becd8
cspears2002 <cspears2002@yahoo.com>
2013-09-26 17:24:13 -0700

    Can detect winners for the columns and rows.  Need to work on the diagonals.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
+function resetGame(element){
+	element.style.background = '#ff0000';
+}
+
 function addXO(element){
-	//console.log(element)
+	console.log("hello world" + element);
+	
 	var rd_plyer_1 = document.getElementById('player_1');
 	var rd_plyer_2 = document.getElementById('player_2');
 
@@ -10,17 +15,34 @@ function addXO(element){
 		element.innerHTML = "O";
 }
 
-function resetGame(element){
-	element.style.background = '#ff0000';
-}
-
-function navCells() {
-	for (var c = 1; c <= 3 ; ++c)
+function changeSquaresOnClick() {
+	var cellArray = [["","",""],["","",""],["","",""]];
+	for (var r = 1; r <= 3 ; ++r)
 	{
-		for(var r = 1; r <= 3; ++r) 
+		for(var c = 1; c <= 3; ++c) 
 		{
-			var div_id = "box" + c + "_" + r;
-			document.getElementById(div_id).onclick = addXO;
+			var div_id = "box" + r + "_" + c;
+			cellArray[r-1][c-1] = document.getElementById(div_id).innerHTML;
+			//document.getElementById(div_id).onclick = addXO(div_id);
 		}
 	}
+
+	// Test columns.
+	for(var c = 1; c <= 3; ++c)
+		if(cellArray[0][c-1] == cellArray[1][c-1]  && 
+		   cellArray[1][c-1] == cellArray[2][c-1]  && 
+		   cellArray[0][c-1] != "")
+		{
+			alert(cellArray[0][c-1] + " won in row " + c);
+		}
+
+	// Test rows.
+	for(var r = 1; r <= 3; ++r)
+		if(cellArray[r-1][0] == cellArray[r-1][1]  && 
+		   cellArray[r-1][1] == cellArray[r-1][2]  && 
+		   cellArray[r-1][0] != "")
+		{
+			alert(cellArray[r-1][0] + " won in row " + r);
+		}
+	
 }
