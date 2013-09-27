0dc214d8f346212bd6c66eed001b681e448eccd5
cspears2002 <cspears2002@yahoo.com>
2013-09-26 18:13:51 -0700

    Will detect a win as the player plays, but will let player continue playing.

--- scripts/script.js
+++ scripts/script.js
@@ -13,9 +13,11 @@ function addXO(element){
 
 	if (rd_plyer_2.checked && element.innerHTML != "X")
 		element.innerHTML = "O";
+
+	identifyWin();
 }
 
-function changeSquaresOnClick() {
+function identifyWin() {
 	var cellArray = [["","",""],["","",""],["","",""]];
 	for (var r = 1; r <= 3 ; ++r)
 	{
@@ -34,7 +36,7 @@ function changeSquaresOnClick() {
 		   cellArray[1][c-1] == cellArray[2][c-1]  && 
 		   cellArray[0][c-1] != "")
 		{
-			alert(cellArray[0][c-1] + " won in row " + c);
+			alert(cellArray[0][c-1] + " won in column " + c);
 		}
 	}
 
